c00e2c3926ca281c9b404601522811d9ec360086
Igor Ovinov <market1942@mail.ru>
2019-07-08 00:07:21 +0300

    Updates

---
+++ precache-manifest.5d323314271e1fcef6e2fc50b9b14f75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e09f81e6508d98c4ed2539a6cb5228",
+    "url": "/todoreact/index.html"
+  },
+  {
+    "revision": "62ff1b84611f564dc31d",
+    "url": "/todoreact/static/css/main.42a3fd50.chunk.css"
+  },
+  {
+    "revision": "95b78d2c6ad01190ec9c",
+    "url": "/todoreact/static/js/2.3dafed6a.chunk.js"
+  },
+  {
+    "revision": "62ff1b84611f564dc31d",
+    "url": "/todoreact/static/js/main.f488d112.chunk.js"
+  },
+  {
+    "revision": "1a79e8c91cec2ccf546b",
+    "url": "/todoreact/static/js/runtime~main.52893577.js"
+  }
+]);
